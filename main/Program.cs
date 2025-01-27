6942d030619624ec5237297346515fe88295d7ad
ItsDerpyMan <rendszergazda@blathy.info>
2025-01-27 09:40:51 +0100

    commiting the previous commit

--- main/Program.cs
+++ main/Program.cs
@@ -7,7 +7,7 @@ namespace MineSweeper
 
     class MineSweeper
     {
-        public const int field_size = 11;
+        public const int field_size = 30;
         public static int number_of_mines = 10;
         public static int lives = 9;
         public static int round = 1;
@@ -32,7 +32,7 @@ namespace MineSweeper
                 // Game loop
                 // Every iteration is a round. Each round the user has to guess a cordinate on the board.
                 Console.Clear();
-                while (lives > 1)
+                while (lives > 0)
                 {
                     // Listening to Specific keypresses as long as its true
                     bool loop = true;
@@ -55,10 +55,10 @@ namespace MineSweeper
                                     MineSweeper.lives--;
                                     loop = false;
                                     break;
-                                case ConsoleKey.UpArrow:
+                                case ConsoleKey.DownArrow:
                                     if (f.Y > field_size * -1 / 2) f.Y -= 1;
                                     break;
-                                case ConsoleKey.DownArrow:
+                                case ConsoleKey.UpArrow:
                                     if (f.Y < field_size / 2) f.Y += 1;
                                     break;
                                 case ConsoleKey.LeftArrow:
@@ -79,12 +79,16 @@ namespace MineSweeper
                     round++;
                 }
                 Console.SetCursorPosition(0, 19);
-                Console.WriteLine("Congrat u won!!");
+                if (lives > 0)
+                    Console.WriteLine("Congrat u won!!");
+                else Console.WriteLine("You lost!");
                 Console.CursorVisible = true;
                 Console.ReadKey();
             }
             catch (Exception e)
-            {
+            {   
+                Console.Clear();
+                Console.CursorVisible = true;
                 Console.WriteLine(e);
 
                 Console.WriteLine("\nPress any <key> to continue.");
@@ -114,7 +118,7 @@ namespace MineSweeper
             }
             grid.AppendLine();
 
-            for (int y = start; y <= end; y++)
+            for (int y = end; y >= start; y--)
             {
                 grid.Append(y.ToString().PadLeft(3));
                 for (int x = start; x <= end; x++)
@@ -150,7 +154,7 @@ namespace MineSweeper
             const int end = field_size / 2;
             int columnWidth = Math.Max(end.ToString().Length, (-start).ToString().Length) + 1;
 
-            Console.SetCursorPosition(5 + field_size * columnWidth, 4);
+            Console.SetCursorPosition(7 + field_size * columnWidth, 4);
             Console.Write("Rows: ");
 
             int cord_y = start + 1;
@@ -159,7 +163,7 @@ namespace MineSweeper
             {
                 for (int i = 0; i < lrows; i++)
                 {
-                    Console.SetCursorPosition(5 + field_size * columnWidth, cord_y + 9);
+                    Console.SetCursorPosition(7 + field_size * columnWidth, cord_y + 9);
                     Console.Write(new String(' ', 20));
                     cord_y++;
                 }
@@ -168,14 +172,14 @@ namespace MineSweeper
             for (int i = 0; i < y_mines.Count; i++)
             {
 
-                Console.SetCursorPosition(5 + field_size * columnWidth, cord_y + 9);
+                Console.SetCursorPosition(7 + field_size * columnWidth, cord_y + 9);
                 double distance = Field.Veclength(player, y_mines.ElementAt(i));
                 Console.Write($"Mine in a Dist: {distance:F2} ");
                 cord_y++;
             }
             lrows = y_mines.Count;
 
-            Console.SetCursorPosition(5 + field_size * columnWidth, 10);
+            Console.SetCursorPosition(7 + field_size * columnWidth, 10);
             Console.Write("Columns: ");
 
             int cord_x = start + 1;
@@ -184,7 +188,7 @@ namespace MineSweeper
             {
                 for (int i = 0; i < lcolumn; i++)
                 {
-                    Console.SetCursorPosition(5 + field_size * columnWidth, cord_x + 15);
+                    Console.SetCursorPosition(7 + field_size * columnWidth, cord_x + 15);
                     Console.Write(new String(' ', 20));
                     cord_x++;
                 }
@@ -193,7 +197,7 @@ namespace MineSweeper
             for (int i = 0; i < x_mines.Count; i++)
             {
 
-                Console.SetCursorPosition(5 + field_size * columnWidth, cord_x + 15);
+                Console.SetCursorPosition(7 + field_size * columnWidth, cord_x + 15);
                 double distance = Field.Veclength(player, x_mines.ElementAt(i));
                 Console.Write($"Mine in a Dist: {distance:F2} ");
                 cord_x++;
